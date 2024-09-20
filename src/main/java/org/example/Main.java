c1bed6e42311388e0032b14448b07f4389f8fb80
simon864 <121446009+simon864@users.noreply.github.com>
2024-09-20 15:55:37 +0700

    add test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,84 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public Main() {
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static String fizzbuzz(int number) {
+        if (number % 5 == 0 && number % 7 == 0) {
+            return "fizzbuzz";
+        } else if (number % 5 == 0) {
+            return "fizz";
+        } else {
+            return number % 7 == 0 ? "buzz" : Integer.toString(number);
+        }
+    }
+
+    public static String reverseStr(String string) {
+        String result = "";
+
+        for(int index = string.length() - 1; index >= 0; --index) {
+            result = result + string.charAt(index);
+        }
+
+        return result;
+    }
+
+    public static double[] quadraticEquation(double a, double b, double c) throws Exception {
+        double D = b * b - 4.0 * a * c;
+        double[] x;
+
+        if (a == 0) {
+            throw new Exception("Its not quadratic equation");
+        }
+
+        if (D > 0.0) {
+            x = new double[]{(-b + Math.sqrt(D)) / (2.0 * a), (-b - Math.sqrt(D)) / (2.0 * a)};
+            return x;
+        } else if (D == 0.0) {
+            x = new double[]{-b / (2.0 * a)};
+            return x;
+        } else {
+            throw new Exception("There are no real roots");
+        }
+    }
+
+    public static double sumSerias() {
+        double n = 2.0;
+        double sum = 0.0;
+        double memberSerias;
+
+        do {
+            memberSerias = 1.0 / (n * n + n - 2.0);
+            sum += memberSerias;
+            n += 1.0;
+        } while(memberSerias >= Math.pow(10.0, -6.0));
+
+        return sum;
+    }
+
+    public static boolean polindrom(String string) {
+        int half;
+        string = string.toLowerCase();
+
+        if (string.length() % 2 == 0) {
+            half = string.length() / 2;
+        } else {
+            half = (string.length() - 1) / 2;
+        }
+
+        for (int index = 0; index < half; ++index) {
+            if (string.charAt(index) != string.charAt(string.length() - 1 - index)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        for (int number = 1; number <= 500; ++number) {
+            System.out.println(fizzbuzz(number));
         }
     }
 }
--- src/test/java/org/example/MainTest1.java
+++ src/test/java/org/example/MainTest1.java
@@ -1,4 +1,50 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class MainTest1 {
-  
+
+class MainTest {
+
+    @Test
+    void fizzbuzz() {
+        assertEquals("fizzbuzz", Main.fizzbuzz(35));
+        assertEquals("buzz", Main.fizzbuzz(14));
+        assertEquals("fizz", Main.fizzbuzz(10));
+        assertEquals("41", Main.fizzbuzz(41));
+    }
+
+    @Test
+    void reverseStr() {
+        assertEquals("llatsni ekam", Main.reverseStr("make install"));
+    }
+
+    @Test
+    void quadraticEquation() throws Exception {
+        try {
+            Main.quadraticEquation(1, -5, 9);
+        } catch (Exception ex) {
+            assertEquals("There are no real roots", ex.getMessage());
+        }
+
+        try {
+            Main.quadraticEquation(0, -3, 4);
+        } catch (Exception ex) {
+            assertEquals("Its not quadratic equation", ex.getMessage());
+        }
+
+        assertEquals(2, Main.quadraticEquation(1, -4, 4)[0]);
+        assertArrayEquals(new double[] { 1, -4 }, Main.quadraticEquation(1, 3, -4));
+    }
+
+    @Test
+    void sumSerias() {
+        assertEquals(0.6101121094474383, Main.sumSerias());
+    }
+
+    @Test
+    void polindrom() {
+        assertTrue(Main.polindrom("TeNET"));
+        assertFalse(Main.polindrom("bruh"));
+    }
 }
